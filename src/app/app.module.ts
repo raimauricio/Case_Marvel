f9c8954bc012c6bd479babab485d7d86df256200
RaiMauricio <rai_mauricio@hotmail.com>
2021-11-25 15:01:25 -0300

    Criado componente entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
+import { LoginComponent } from './entry/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
