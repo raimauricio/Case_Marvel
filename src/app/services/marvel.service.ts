3a3e68b47ea79a9d6fe79469dc3a289fedbd9eaf
RaiMauricio <rai_mauricio@hotmail.com>
2021-12-06 11:30:52 -0300

    Ajuste serviço de cadastro

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Perfil } from './../interfaces/perfil.component';
 import { Injectable } from '@angular/core';
+
+import { Perfil } from './../interfaces/perfil.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,15 @@ import { Injectable } from '@angular/core';
 
 export class MarvelService {
 
+  static qtdPerfilReg = 1;
   perfis: Perfil[] = [
-    {nickname:'admin',firstName:'', lastName:'', email:'', contact:'', password:'admin', cardNumber:''
+    {id:0, nickname:'admin',firstName:'', lastName:'', email:'', contact:'', password:'admin', cardNumber:''
     , validity:0, cvv:0, cardName: '', cardCpf:'', cep: 0, address:'', addressNumber: 0, complement: '',
     district:'', city:''}
   ];
 
-  perfilTemp = {nickname:'',firstName:'', lastName:'', email:'', contact:'', password:'', cardNumber:''
-    , validity:0, cvv:0, cardName: '', cardCpf:'', cep: 0, address:'', addressNumber: 0, complement: '',
-    district:'', city:'' };
+  nickname =''; firstname =''; lastname='';email=''; contact=''; password='';cardnumber='' ; validity=0;
+  cvv=0; cardname=''; cardcpf='';cep=0;address='';addressnumber=0;complement='';district='';city='';
 
   constructor(private http: HttpClient) { }
 
@@ -28,33 +29,45 @@ export class MarvelService {
   }
 
   cadastrandoAccount(perfil: any){
-    this.perfilTemp.nickname = perfil.nickname;
-    this.perfilTemp.firstName = perfil.firstname;
-    this.perfilTemp.lastName = perfil.lastname;
-    this.perfilTemp.email = perfil.email;
-    this.perfilTemp.contact = perfil.contact;
-    this.perfilTemp.password = perfil.password;
+
+    this.nickname = perfil.nickname;
+    this.firstname = perfil.firstname;
+    this.lastname = perfil.lastname;
+    this.email = perfil.email;
+    this.contact = perfil.contact;
+    this.password = perfil.password;
 
   }
 
   cadastrandoPayment(perfil: any){
-    this.perfilTemp.cardNumber = perfil.cardNumber;
-    this.perfilTemp.validity = perfil.validity;
-    this.perfilTemp.cvv = perfil.cvv;
-    this.perfilTemp.cardName = perfil.cardName;
-    this.perfilTemp.cardCpf = perfil.cpf;
+    this.cardnumber = perfil.cardNumber;
+    this.validity = perfil.validity;
+    this.cvv = perfil.cvv;
+    this.cardname = perfil.cardName;
+    this.cardcpf = perfil.cpf;
 
   }
 
   cadastrandoAddress(perfil: any){
-    this.perfilTemp.cep = perfil.cep;
-    this.perfilTemp.address = perfil.address;
-    this.perfilTemp.addressNumber = perfil.numberAddress;
-    this.perfilTemp.complement= perfil.complement;
-    this.perfilTemp.district = perfil.district;
-    this.perfilTemp.city = perfil.city;
 
-    this.perfis.push(this.perfilTemp);
+    this.cep = perfil.cep;
+    this.address = perfil.address;
+    this.addressnumber = perfil.numberAddress;
+    this.complement= perfil.complement;
+    this.district = perfil.district;
+    this.city = perfil.city;
+
+    let perfilNovo = {
+      id: MarvelService.qtdPerfilReg,
+      nickname: this.nickname, firstName: this.firstname, lastName: this.lastname,email: this.email,
+      contact: this.contact, password: this.password, cardNumber: this.cardnumber, validity: this.validity,
+      cvv: this.cvv, cardName: this.cardname, cardCpf: this.cardcpf, cep: this.cep, address: this.address,
+      addressNumber: this.addressnumber, complement: this.complement, district: this.district, city: this.city
+    } ;
+
+    this.perfis.push(perfilNovo);
+    MarvelService.qtdPerfilReg++;
+    console.log(this.perfis);
 
   }
 
