3edbd7569d2b259a7887df8d466b8c0796ea4bd8
RaiMauricio <rai_mauricio@hotmail.com>
2021-12-08 18:10:39 -0300

    Finalizado template book-details

--- src/app/home/home-route.routing.ts
+++ src/app/home/home-route.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ComicBooksComponent } from './comic-books/comic-books.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
     children:[
       {path:'comic-books', component: ComicBooksComponent},
       {path:'profile', component: ProfileComponent},
+      {path: 'book-details', component:BookDetailsComponent},
       {path:'', component: ComicBooksComponent}
     ]
   }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './home.component';
 import { ComicBooksComponent } from './comic-books/comic-books.component'
 import { HomeRouteRoutes } from './home-route.routing';
 import { ProfileComponent } from './profile/profile.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     ComicBooksComponent,
-    ProfileComponent
+    ProfileComponent,
+    BookDetailsComponent
   ],
   exports:[HomeComponent],
   imports: [
